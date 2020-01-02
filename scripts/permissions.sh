29a3cac2c07765ae9ecf922421ebc13aad7861d9
Brian Simoni <simonibc@vcu.edu>
2020-01-02 23:49:44 +0000

    Add permissions script

--- scripts/permissions.sh
+++ scripts/permissions.sh
@@ -1,8 +1,8 @@
-chgrp -R daemon /home/bitnammi/wordpress/wp-content/themes/industrial
-chown -R bitnami /home/bitnammi/wordpress/wp-content/themes/industrial
+chgrp -R daemon /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial
+chown -R bitnami /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial
 
-chgrp -R daemon /home/bitnammi/wordpress/wp-content/themes/industrial-child
-chown -R bitnami /home/bitnammi/wordpress/wp-content/themes/industrial-child
+chgrp -R daemon /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial-child
+chown -R bitnami /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial-child
 
-chmod -R 664 /home/bitnammi/wordpress/wp-content/themes/industrial
-chmod -R 664 /home/bitnammi/wordpress/wp-content/themes/industrial-child
+chmod -R 664 /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial
+chmod -R 664 /opt/bitnami/apps/wordpress/htdocs/wp-content/themes/industrial-child
